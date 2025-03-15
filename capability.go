dae5d6b8b530558eb09472eb4fec39ca20390c12
Ryan Fan <reg_info@qq.com>
2025-03-15 15:20:48 +0800

    change provider (#2)

--- capability.go
+++ capability.go
@@ -2,7 +2,6 @@ package mysql_sqlboiler
 
 import (
 	"github.com/hdget/common/types"
-	"github.com/hdget/provider-mysql-sqlboiler/pkg"
 	"go.uber.org/fx"
 )
 
@@ -10,11 +9,11 @@ const (
 	providerName = "mysql-sqlboiler"
 )
 
-var Capability = &types.Capability{
+var Capability = types.Capability{
 	Category: types.ProviderCategoryDb,
 	Name:     providerName,
 	Module: fx.Module(
 		providerName,
-		fx.Provide(pkg.New),
+		fx.Provide(New),
 	),
 }
--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-package pkg
+package mysql_sqlboiler
 
 import (
 	"github.com/hdget/common/intf"
